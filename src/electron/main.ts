2569cc3a97f74b8530f5ac724a7f8bb1fb487413
Sarthak <messagetosarthak@gmail.com>
2026-01-21 01:10:30 +0530

    Electron Store Implemented to Save Data for The Entire Day

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -4,22 +4,38 @@ import { isDev } from './util.js';
 import { getPreloadPath } from './pathResolver.js';
 import { ipcMain, powerMonitor } from 'electron';
 import  activeWindow  from 'active-win'; 
+import Store from 'electron-store'
 let intervalId: any;  
-const screenTime: any = {}
+let todaySessionDetails: any = {}
 let previousWindow: any = null; 
-let contextSwitch: any = 0; //to account for the number of context switches 
-
+let contextSwitch: any = 0; //to account for the number of context switches
+let saveCounter = 0;  
+const store = new Store();
+//for the logic of which object we will return to the renderer process. ]
+//we have two sub problems here
+//first we will set today as the current date,and then get new date everytime the function runs. if the dates are same then we will not change the today session details. if they are different we will change it 
+//the next part is about how to reset it automatically at midnight.
+//also there is one nuance as to what will happen if the user pauses or ends the session. 
+let date = new Date();
+let today = date.toLocaleDateString();
+let currentdate = store.get('current-date');
+console.log(today, currentdate)
+if(currentdate === today) {
+  console.log(store.get('session-details'))
+  todaySessionDetails = store.get('session-details')
+  console.log(todaySessionDetails)
+}
 app.on('ready', () => {
   const mainWindow = new BrowserWindow({
-  width: 350,
-  height: 600,
+    width: 350,
+    height: 600,
   resizable: false,  
   webPreferences: {
   preload: getPreloadPath(), 
 }
-  });
-  mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'));
-  mainWindow.setAlwaysOnTop(true);
+});
+mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'));
+mainWindow.setAlwaysOnTop(true);
   if (isDev()) {
     mainWindow.loadURL('http://localhost:5123');
   } else {
@@ -31,14 +47,15 @@ app.on('ready', () => {
   ipcMain.handle('start-session-time-tracking',  startSessionTracking); 
   ipcMain.handle('get-screen-time', () => {
     clearInterval(intervalId);
-    screenTime["contextSwitch"] = contextSwitch;  
-    return screenTime;
+    todaySessionDetails["contextSwitch"] = contextSwitch;  
+    store.set('session-details', todaySessionDetails)
+    return todaySessionDetails;
   })
   })
 
 
-//hmr only works for react files. if you make any changes to the electron code then it needs to be restarted.
-// //the current problems which are left to be solved are. 
+  //hmr only works for react files. if you make any changes to the electron code then it needs to be restarted.
+  // //the current problems which are left to be solved are. 
 // - how do we calculate the data of the entire day 
 // - how do you store it in an object such that a new object is created each day. once this is resolved, we can move on to how to display the data. 
 // - in terms of ui/Ux, tinker how the data needs to be displayed. is it going to be day wise, or it is going to be focus session wise, or is it going to be both. 
@@ -46,27 +63,31 @@ app.on('ready', () => {
 function startSessionTracking() { 
     intervalId = setInterval(async () => {
       try {
-        console.log(screenTime)
+        saveCounter++; 
         const presentWindow = await activeWindow(); 
         if(!presentWindow) return; 
         if(!previousWindow) //this is the first time the interval is running, incrementing the screentime by 2 to account for the loss of the time the interval was absent.  
         {
           const cleanTitle: any = presentWindow.title.split(' - ').pop(); 
-          screenTime[cleanTitle] = ((screenTime[cleanTitle]) || 0) + 2;
+          todaySessionDetails[cleanTitle] = ((todaySessionDetails[cleanTitle]) || 0) + 2;
           previousWindow = cleanTitle; 
           return; 
         }
-        console.log(presentWindow) //remove this after testing is complete
         const cleanTitle: any = presentWindow.title.split(' - ').pop(); 
-        screenTime[cleanTitle] = ((screenTime[cleanTitle]) || 0) + 1; 
+        todaySessionDetails[cleanTitle] = ((todaySessionDetails[cleanTitle]) || 0) + 1; 
         if (previousWindow === cleanTitle) {
           return; 
         } else {
           contextSwitch++; 
           previousWindow = cleanTitle; 
         }
+        if (saveCounter >= 12) {
+          store.set('session-details', todaySessionDetails);
+          saveCounter = 0; 
+        }
       } catch (error) { 
         console.error("failed to fetch active window", error); 
       }
     }, 5000)
 }
+        store.set('current-date', today);
